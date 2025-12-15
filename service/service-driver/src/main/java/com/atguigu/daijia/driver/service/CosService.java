5af4fb9f05ad9fd935682a31bed5618fb88ba954
zyrwl241 <1660985901@qq.com>
2025-12-15 15:15:14 +0800

    1.3

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/CosService.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/CosService.java
@@ -1,6 +1,11 @@
 package com.atguigu.daijia.driver.service;
 
+import com.atguigu.daijia.model.vo.driver.CosUploadVo;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface CosService {
+    //文件上传
+    CosUploadVo upload(MultipartFile file, String path);
 
-
+    String getImageUrl(String path);
 }
--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
@@ -1,13 +1,29 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.driver.client.CosFeignClient;
 import com.atguigu.daijia.driver.service.CosService;
+import com.atguigu.daijia.model.vo.driver.CosUploadVo;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CosServiceImpl implements CosService {
 
+    @Autowired
+    private CosFeignClient cosFeignClient;
+
+    //文件上传接口
+    @Override
+    public CosUploadVo upload(MultipartFile file, String path) {
+        //远程调用
+        Result<CosUploadVo> cosUploadVoResult = cosFeignClient.upload(file,path);
+        return cosUploadVoResult.getData();
+    }
 
 }
