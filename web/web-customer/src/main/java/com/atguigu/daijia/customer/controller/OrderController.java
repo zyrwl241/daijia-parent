dccff287e09f2a8040e068e57d647fde26d4c718
zyrwl241 <1660985901@qq.com>
2025-12-17 19:16:31 +0800

    2025-12-17first commit

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
@@ -1,11 +1,16 @@
 package com.atguigu.daijia.customer.controller;
 
+import com.atguigu.daijia.common.login.GuiguLogin;
+import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.customer.service.OrderService;
+import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
+import com.atguigu.daijia.model.vo.order.CurrentOrderInfoVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Tag(name = "订单API接口管理")
@@ -14,6 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
+    @Operation(summary = "预估订单数据")
+    @GuiguLogin
+    @PostMapping("/expectOrder")
+    public Result<ExpectOrderVo> expectOrder(@RequestBody ExpectOrderForm expectOrderForm) {
+        return Result.ok(orderService.expectOrder(expectOrderForm));
+    }
+
+    @Operation(summary = "查找乘客端当前订单")
+    @GuiguLogin
+    @GetMapping("/searchCustomerCurrentOrder")
+    public Result<CurrentOrderInfoVo> searchCustomerCurrentOrder() {
+        CurrentOrderInfoVo currentOrderInfoVo = new CurrentOrderInfoVo();
+        currentOrderInfoVo.setIsHasCurrentOrder(false);
+        return Result.ok(currentOrderInfoVo);
+    }
 
 }
 
--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
@@ -1,12 +1,54 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.customer.service.OrderService;
+import com.atguigu.daijia.map.client.MapFeignClient;
+import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.form.rules.FeeRuleRequestForm;
+import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
+import com.atguigu.daijia.model.vo.map.DrivingLineVo;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
+import com.atguigu.daijia.rules.client.FeeRuleFeignClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    private MapFeignClient mapFeignClient;
+    @Autowired
+    private FeeRuleFeignClient feeRuleFeignClient;
+
+
+    //预估订单数据
+    @Override
+    public ExpectOrderVo expectOrder(ExpectOrderForm expectOrderForm) {
+        //获取驾驶线路
+        CalculateDrivingLineForm calculateDrivingLineForm = new CalculateDrivingLineForm();
+        BeanUtils.copyProperties(expectOrderForm,calculateDrivingLineForm);
+        Result<DrivingLineVo> drivingLineVoResult = mapFeignClient.calculateDrivingLine(calculateDrivingLineForm);
+        DrivingLineVo drivingLineVo = drivingLineVoResult.getData();
+
+        //获取订单费用
+        FeeRuleRequestForm calculateOrderFeeForm = new FeeRuleRequestForm();
+        calculateOrderFeeForm.setDistance(drivingLineVo.getDistance());
+        calculateOrderFeeForm.setStartTime(new Date());
+        calculateOrderFeeForm.setWaitMinute(0);
+        Result<FeeRuleResponseVo> feeRuleResponseVoResult = feeRuleFeignClient.calculateOrderFee(calculateOrderFeeForm);
+        FeeRuleResponseVo feeRuleResponseVo = feeRuleResponseVoResult.getData();
+
+        //封装ExpectOrderVo
+        ExpectOrderVo expectOrderVo = new ExpectOrderVo();
+        expectOrderVo.setDrivingLineVo(drivingLineVo);
+        expectOrderVo.setFeeRuleResponseVo(feeRuleResponseVo);
+        return expectOrderVo;
+    }
 }
