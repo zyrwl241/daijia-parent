8d7ca7fd63d399d5bb4cf892638433348ce2b2d8
zyrwl241 <1660985901@qq.com>
2025-12-29 15:56:21 +0800

    2025-12-29third commit

--- service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderMonitorController.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderMonitorController.java
@@ -1,15 +1,26 @@
 package com.atguigu.daijia.order.controller;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
+import com.atguigu.daijia.model.form.order.OrderMonitorForm;
 import com.atguigu.daijia.order.service.OrderMonitorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/order/monitor")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderMonitorController {
 
+    @Autowired
+    private OrderMonitorService orderMonitorService;
 
+    @Operation(summary = "保存订单监控记录数据")
+    @PostMapping("/saveOrderMonitorRecord")
+    public Result<Boolean> saveMonitorRecord(@RequestBody OrderMonitorRecord orderMonitorRecord) {
+        return Result.ok(orderMonitorService.saveOrderMonitorRecord(orderMonitorRecord));
+    }
 }
 
--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
@@ -1,13 +1,37 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.driver.service.FileService;
 import com.atguigu.daijia.driver.service.MonitorService;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
+import com.atguigu.daijia.model.form.order.OrderMonitorForm;
+import com.atguigu.daijia.order.client.OrderMonitorFeignClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class MonitorServiceImpl implements MonitorService {
+    @Autowired
+    private FileService fileService;
 
+    @Autowired
+    private OrderMonitorFeignClient orderMonitorFeignClient;
 
+    @Override
+    public Boolean upload(MultipartFile file, OrderMonitorForm orderMonitorForm) {
+        //上传对话文件
+        String url = fileService.upload(file);
+        log.info("upload: {}", url);
+
+        //保存订单监控记录数
+        OrderMonitorRecord orderMonitorRecord = new OrderMonitorRecord();
+        orderMonitorRecord.setOrderId(orderMonitorForm.getOrderId());
+        orderMonitorRecord.setFileUrl(url);
+        orderMonitorRecord.setContent(orderMonitorForm.getContent());
+        orderMonitorFeignClient.saveMonitorRecord(orderMonitorRecord);
+        return true;
+    }
 }
